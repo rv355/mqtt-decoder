ae54176e97ba983df686c13a58a21093385f9694
rv355 <rv355@cam.ac.uk>
2020-03-26 15:43:03 +0000

    upload

---
+++ README.md
@@ -0,0 +1,24 @@
+This project is for decoding the MQTT messages we receive from feedmqtt and publish it with additional fields.
+
+### Features
+The current version subscribes to feedmqtt.local, extracts the sensor id and publishes on the same channel under the topic "tfc_prod/sensor-id".
+
+### Getting Started
+
+## Prerequisites
++ python paho-mqtt
++ >=Python3
+
+## Running
+
+Once you have updated the required fields in config_local.json, run the following command to start the decoder.
+
+```
+python feedmqtt_decoder.py
+```
+
+You could see the published messages using a mqtt client like mosquitto.
+
+```
+mosquitto_sub -h hostname -u username -P password -u 'tfc_prod/#'
+```
---
+++ feedmqtt_decoder.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import json
+
+with open('config_local.json') as json_file:
+    config = json.load(json_file)
+
+def get_msg_to_publish(msg):
+    sid = msg.topic.split('/')[1]
+    publish_msg = '{\"acp_id\":\"'+sid+'\", \"payload\":' + str(msg.payload.decode("utf-8")+'}')
+    #print(publish_msg)
+    return(sid,publish_msg)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(config['mqttTopic'])
+
+def on_message(client, userdata, msg):
+    #print(msg.topic+" "+str(msg.payload.decode("utf-8")))
+    #print(msg.topic)
+    sensor_id, pub_msg = get_msg_to_publish(msg)
+    topic = 'tfc_prod/'+sensor_id
+    #print(topic,pub_msg)
+    pubClient = mqtt.Client()
+    pubClient.connect('localhost', 1883)
+    ret = pubClient.publish(topic, pub_msg)
+
+client = mqtt.Client()
+client.username_pw_set(username=config['username'],password=config['password'])
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(config['hostname'])
+
+client.loop_forever()
